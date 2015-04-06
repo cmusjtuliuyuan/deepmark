fae0db1d557000ce09c89b3bd9cbbadb01a0e23f
seaofocean <jiaxianyan@gmail.com>
2015-04-07 03:29:04 +0800

    change model structure

--- torch/audio/cudnn_deepspeech2.lua
+++ torch/audio/cudnn_deepspeech2.lua
@@ -8,25 +8,25 @@ end
 local function cudnnDeepSpeech2(miniBatchSize, freqBins, nGPUs, useOptnet)
 
     local model = nn.Sequential()
-    model:add(cudnn.SpatialConvolution(1, 32, 20, 5, 2, 2))
-    model:add(cudnn.SpatialBatchNormalization(32))
-    model:add(cudnn.ClippedReLU(20, true))
-    model:add(cudnn.SpatialConvolution(32, 32, 10, 5, 2, 1))
-    model:add(cudnn.SpatialBatchNormalization(32))
+    model:add(cudnn.SpatialConvolution(1, 1152, 11, 13, 3, 1))
+    model:add(cudnn.SpatialBatchNormalization(1152))
     model:add(cudnn.ClippedReLU(20, true))
 
-    model:add(nn.View(32 * 75, -1):setNumInputDims(3)) -- batch x features x seqLength
+    model:add(nn.View(1152*((161-13)/1+1), -1):setNumInputDims(3)) -- batch x features x seqLength
     model:add(nn.Transpose({ 2, 3 }, { 1, 2 })) -- seqLength x batch x features
 
-    model:add(cudnn.BatchBRNNReLU(32 * 75, 1760))
-    model:add(cudnn.BatchBRNNReLU(1760, 1760))
-    model:add(cudnn.BatchBRNNReLU(1760, 1760))
-    model:add(cudnn.BatchBRNNReLU(1760, 1760))
-    model:add(cudnn.BatchBRNNReLU(1760, 1760))
-    model:add(cudnn.BatchBRNNReLU(1760, 1760))
-    model:add(cudnn.BatchBRNNReLU(1760, 1760))
+    model:add(cudnn.BatchBRNNReLU(1152*((161-13)/1+1), 1152))
+    model:add(cudnn.BatchBRNNReLU(1152, 1152))
+    model:add(cudnn.BatchBRNNReLU(1152, 1152))
+    model:add(cudnn.BatchBRNNReLU(1152, 1152))
+    model:add(cudnn.BatchBRNNReLU(1152, 1152))
+    model:add(cudnn.BatchBRNNReLU(1152, 1152))
+    model:add(cudnn.BatchBRNNReLU(1152, 1152))
+    model:add(cudnn.BatchBRNNReLU(1152, 1152))
+    model:add(cudnn.BatchBRNNReLU(1152, 1152))
 
-    model:add(nn.Bottle(nn.Linear(1760, 29))) -- keeps the output 3D for multi-GPU.
+    model:add(nn.Bottle(nn.Linear(1152, 1152))) -- keeps the output 3D for multi-GPU.
+    model:add(nn.Bottle(nn.Linear(1152, 29)))
     model:cuda()
     if useOptnet then
 	local optnet = require 'optnet'
